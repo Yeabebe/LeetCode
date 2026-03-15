86ab428d828ac2eac40064d7de2733564d8ceb10
Yeabseera Abebe <yeabebe41@gmail.com>
2026-03-15 13:39:50 +0300

    The skyline problem

---
+++ skyline_problem.py
@@ -0,0 +1,24 @@
+class Solution:
+    def getSkyline(self, buildings: List[List[int]]) -> List[List[int]]:
+        events = []
+
+        for L, R, H in buildings:
+            events.append((L, -H, R))
+            events.append((R, 0, 0))       
+        events.sort()
+
+        res=[[0, 0]]
+        heap = [(0, float('inf'))] 
+
+        for x, negH, R in events:
+            while heap[0][1] <=x:
+                heapq.heappop(heap)
+
+            if negH != 0:
+                heapq.heappush(heap, (negH, R))
+
+            curr_height = -heap[0][0]
+            if res[-1][1] != curr_height:
+                res.append([x, curr_height])   
+        return res[1:]
+
