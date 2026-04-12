a149b96842683e4a2f48eefb1857402edda2bd26
Yeabseera Abebe <yeabebe41@gmail.com>
2026-04-12 16:16:36 +0300

    Single number III

---
+++ single_numberIII.py
@@ -0,0 +1,23 @@
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> List[int]:
+        xor = 0
+        
+        # Step 1: XOR all numbers
+        for num in nums:
+            xor ^= num
+        
+        # Step 2: Find rightmost set bit
+        diff = xor & -xor
+        
+        a = 0
+        b = 0
+        
+        # Step 3: Split into two groups
+        for num in nums:
+            if num & diff:
+                a ^= num
+            else:
+                b ^= num
+        
+        return [a, b]
