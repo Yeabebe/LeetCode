51878298e5603f125ddd9c59d57b3f4fc1e921d5
Yeabseera Abebe <yeabebe41@gmail.com>
2026-04-30 09:52:08 +0300

    Move zeroes

---
+++ move_zeros.py
@@ -0,0 +1,13 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        insert_pos = 0
+        
+        # Step 1: Move non-zero elements forward
+        for num in nums:
+            if num != 0:
+                nums[insert_pos] = num
+                insert_pos += 1
+        
+        # Step 2: Fill remaining with zeros
+        for i in range(insert_pos, len(nums)):
+            nums[i] = 0
