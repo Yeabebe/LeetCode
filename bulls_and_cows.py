36441375089fa1cdc8740ae396bcfba6bd4fee95
Yeabseera Abebe <yeabebe41@gmail.com>
2026-05-27 09:28:04 +0300

    Bulls and Cows

---
+++ bulls_and_cows.py
@@ -0,0 +1,23 @@
+from collections import Counter
+
+class Solution:
+    def getHint(self, secret: str, guess: str) -> str:
+        bulls = 0
+        
+        secret_count = Counter()
+        guess_count = Counter()
+        
+        # Count bulls
+        for s, g in zip(secret, guess):
+            if s == g:
+                bulls += 1
+            else:
+                secret_count[s] += 1
+                guess_count[g] += 1
+        
+        # Count cows
+        cows = 0
+        for digit in secret_count:
+            cows += min(secret_count[digit], guess_count[digit])
+        
+        return f"{bulls}A{cows}B"
