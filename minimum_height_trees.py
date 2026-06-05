7c3547b0d635efd65ff7f890fa1c3b32205b7d68
Yeabseera Abebe <yeabebe41@gmail.com>
2026-06-05 08:19:15 +0300

    Minimum height tree

---
+++ minimum_height_trees.py
@@ -0,0 +1,37 @@
+from typing import List
+from collections import deque
+
+class Solution:
+    def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
+
+        if n == 1:
+            return [0]
+
+        graph = [set() for _ in range(n)]
+
+        for u, v in edges:
+            graph[u].add(v)
+            graph[v].add(u)
+
+        leaves = deque()
+
+        for i in range(n):
+            if len(graph[i]) == 1:
+                leaves.append(i)
+
+        remaining = n
+
+        while remaining > 2:
+            size = len(leaves)
+            remaining -= size
+
+            for _ in range(size):
+                leaf = leaves.popleft()
+
+                neighbor = graph[leaf].pop()
+                graph[neighbor].remove(leaf)
+
+                if len(graph[neighbor]) == 1:
+                    leaves.append(neighbor)
+
+        return list(leaves)
