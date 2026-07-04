02af895ec9ef03aad74cb11c5b760b8ef4dd441c
Yeabseera Abebe <yeabebe41@gmail.com>
2026-07-04 07:39:37 +0300

    Top K frequent elements

---
+++ frequent_elements.py
@@ -0,0 +1,18 @@
+from collections import Counter,List
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = Counter(nums)
+
+        buckets = [[] for _ in range(len(nums) + 1)]
+
+        for num, freq in count.items():
+            buckets[freq].append(num)
+
+        result = []
+
+        for freq in range(len(nums), 0, -1):
+            for num in buckets[freq]:
+                result.append(num)
+                if len(result) == k:
+                    return result
