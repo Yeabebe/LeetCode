11ee00472d304273d0a41584a74068cd9050975b
Yeabseera Abebe <yeabebe41@gmail.com>
2026-08-05 08:07:32 +0300

    Linked list random node

---
+++ linked_list.py
@@ -0,0 +1,14 @@
+import random
+from typing import Optional, ListNode
+     
+class Solution:
+
+    def __init__(self, head: Optional[ListNode]):
+        self.values = []
+
+        while head:
+            self.values.append(head.val)
+            head = head.next
+
+    def getRandom(self) -> int:
+        return random.choice(self.values)
